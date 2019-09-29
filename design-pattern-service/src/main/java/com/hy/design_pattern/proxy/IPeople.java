9d787f18966b80fd60def4a019a2a967db7a01f8
wanghai <wangh_2@sina.com>
2019-09-29 09:37:49 +0800

    test

---
+++ design-pattern-service/src/main/java/com/hy/design_pattern/proxy/IPeople.java
@@ -0,0 +1,13 @@
+package com.hy.design_pattern.proxy;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/9/28 21:52
+ * @Copyright:reach-life
+ * @Description:
+ */
+public interface IPeople {
+
+    void eat();
+
+}
---
+++ design-pattern-service/src/main/java/com/hy/design_pattern/proxy/ZhangsanPeople.java
@@ -0,0 +1,14 @@
+package com.hy.design_pattern.proxy;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/9/28 21:53
+ * @Copyright:reach-life
+ * @Description:
+ */
+public class ZhangsanPeople implements IPeople {
+    @Override
+    public void eat() {
+        System.out.println("zhangsan eat");
+    }
+}
