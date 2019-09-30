394ed8ee01d87edf1d488aac4442d1c1990e7a32
wanghai <wangh_2@sina.com>
2019-09-30 17:24:01 +0800

    test

---
+++ design-pattern-web/src/test/java/com/hy/Testmain.java
@@ -0,0 +1,14 @@
+package com.hy;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class Testmain {
+
+    @Test
+    public void test1() {
+        System.out.println(LocalDate.now());
+    }
+
+}
