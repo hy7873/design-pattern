0362477aad6cf6f3f029c131fb8023d93e062bc2
wanghai <wangh_2@sina.com>
2019-09-30 14:32:09 +0800

    策略

---
+++ design-pattern-service/src/main/java/com/hy/design_pattern/strategy/AbstractActivityStrategy.java
@@ -0,0 +1,13 @@
+package com.hy.design_pattern.strategy;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/9/30 10:51
+ * @Copyright:reach-life
+ * @Description:抽象策略处理
+ */
+public abstract class AbstractActivityStrategy {
+
+    abstract public void process(String activityType);
+
+}
--- design-pattern-web/src/test/java/com/hy/AppTest.java
+++ design-pattern-web/src/test/java/com/hy/AppTest.java
@@ -1,17 +1,12 @@
 package com.hy;
 
-import com.google.gson.Gson;
 import com.hy.dao.UserMapper;
-import com.hy.model.User;
-import com.hy.service.UserStatusService;
+import com.hy.design_pattern.strategy.ActivityServiceProcessor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,12 +16,21 @@ public class AppTest
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private ActivityServiceProcessor icbcETCActivityService;
+
+
     @Test
     public void test1() {
-        List<User> userList = new ArrayList<>();
+        /*List<User> userList = new ArrayList<>();
         User user = new User();
         user.setMobile("158");
         userList = userMapper.getListByConditionLike(user);
-        System.out.println(new Gson().toJson(userList));
+        System.out.println(new Gson().toJson(userList));*/
+    }
+
+    @Test
+    public void test2() {
+        icbcETCActivityService.process("ccbVolunteer");
     }
 }
